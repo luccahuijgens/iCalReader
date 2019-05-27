5aa8836fd631f0d60603d1317d7736dddafe30c0
lhuijgens <lucca.huijgens@student.hu.nl>
2019-05-27 15:40:05 +0200

    added front-end necessities for testmode

--- src/main/java/iotalarm/dataaccess/TestReader.java
+++ src/main/java/iotalarm/dataaccess/TestReader.java
@@ -15,45 +15,47 @@ import net.fortuna.ical4j.data.ParserException;
 
 public class TestReader {
 	private static TestReader instance;
-	private static List<Event> list=new ArrayList<Event>();
-	private static String url="";
-	
+	private static List<Event> list = new ArrayList<>();
+	private static String url = "";
+
 	private TestReader() {
-		list.add(new Event(1,"Test Event","Test Locatie",getTestDate()));
+		list.add(new Event(1, "Test Event", "Test Locatie", getTestDate().getTime()));
 	}
-	
-	private Date getTestDate() {
-		Date result=new Date();
-		int currentMinutes=result.getMinutes();
-		if (currentMinutes>57) {
+
+	private static void updateEvents() {
+		list = new ArrayList<>();
+		list.add(new Event(1, "Test Event", "Test Locatie", getTestDate().getTime()));
+	}
+
+	private static Date getTestDate() {
+		Date result = new Date();
+		int currentMinutes = result.getMinutes();
+		if (currentMinutes > 57) {
 			result.setHours(result.getHours());
-			result.setMinutes(currentMinutes+2-60);
-		}else {
-			result.setMinutes(currentMinutes+2);
+			result.setMinutes(currentMinutes + 2 - 60);
+		} else {
+			result.setMinutes(currentMinutes + 2);
 		}
 		return result;
 	}
-	
+
 	public static TestReader getInstance() {
-		if (instance==null) {
-			instance=new TestReader();
+		if (instance == null) {
+			instance = new TestReader();
 		}
 		return instance;
 	}
-	
+
 	public void updateUrl(String newurl) throws IOException, ParserException {
-		if (!url.equals(newurl)) {
-			url=newurl;
-		}
+		updateEvents();
 	}
 
-	
-	public static List<Event> getEvents(){
+	public static List<Event> getEvents() {
 		return list;
 	}
-	
-	public static List<Event> getTodaysEvents(){
-		List<Event>result=new ArrayList<Event>();
+
+	public static List<Event> getTodaysEvents() {
+		List<Event> result = new ArrayList<Event>();
 		for (Event e : list) {
 			if (isToday(e.getDate())) {
 				result.add(e);
@@ -61,18 +63,19 @@ public class TestReader {
 		}
 		Collections.sort(result, new Comparator<Event>() {
 			  public int compare(Event o1, Event o2) {
-			      return o1.getDate().compareTo(o2.getDate());
+			      return Long.valueOf(o1.getDate()).compareTo(Long.valueOf(o2.getDate()));
 			  }
 			});
 		return result;
 	}
-	
-	private static boolean isToday(Date eventdate) {
-LocalDate today=LocalDate.now();
-LocalDate parsedEventDate=LocalDate.of(eventdate.getYear()+1900, eventdate.getMonth()+1, eventdate.getDate());
-		if (parsedEventDate.isEqual(today)) {
-			return true;
+
+	private static boolean isToday(long l) {
+		LocalDate today=LocalDate.now();
+		Date lDate = new Date(Long.parseLong(String.valueOf(l)) * 1000);
+		LocalDate parsedEventDate=LocalDate.of(lDate.getYear()+1900, lDate.getMonth()+1, lDate.getDate());
+			if (parsedEventDate.isEqual(today)) {
+				return true;
+			}
+			return false;
 		}
-		return false;
 	}
-}
