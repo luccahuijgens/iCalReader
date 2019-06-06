443c1d76e239e6ff4c01b15e7e8214a758b2c0ab
RandomRobin <rlvvlijmen@gmail.com>
2019-06-06 08:00:58 +0200

    You gota be kiddin me

--- src/main/java/iotalarm/webservices/TestModesResource.java
+++ src/main/java/iotalarm/webservices/TestModesResource.java
@@ -3,19 +3,16 @@ package iotalarm.webservices;
 import javax.json.Json;
 import javax.json.JsonObjectBuilder;
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 import iotalarm.service.TestModesService;
-import iotalarm.service.TravelTimeService;
 
-public class TestModesResource {
-
-	@Path("testmodes")
-	public class TravelTimeResource extends BasicResource{
+@Path("testmodes")
+	public class TestModesResource extends BasicResource{
 		private TestModesService service = new TestModesService();
+		
 		@GET
 		@Produces("application/json")
 		public String getTestModes() {
@@ -43,4 +40,3 @@ public class TestModesResource {
 			}
 		}
 	}
-}
